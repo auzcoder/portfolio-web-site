d05652e8f70a503ab4692971c51d8d28f1fb38c7
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-01-23 21:42:50 +0500

    .

--- talim/admin.py
+++ talim/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 
-# Register your models here.
+from talim.models import Talim
+
+
+@admin.register(Talim)
+class TajribaAdmin(TranslationAdmin):
+    list_display = ['name', 'started_at', 'ended_at']
+
+    class Media:
+        js = (
+            'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
