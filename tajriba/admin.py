7772fa96305302c4ffa32476691cb246d0cc4f55
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-01-23 18:26:00 +0500

    .

--- tajriba/admin.py
+++ tajriba/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from tajriba.models import Tajriba
+
+
+@admin.register(Tajriba)
+class TajribaAdmin(ModelAdmin):
+    list_display = ['name', 'started_at', 'ended_at', 'position']
+
+
