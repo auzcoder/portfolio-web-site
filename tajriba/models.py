1d3f2b75967dd45b51eb272daf4afe6223075966
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-01-23 18:00:46 +0500

    .

--- tajriba/models.py
+++ tajriba/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Tajriba(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
