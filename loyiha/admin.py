630afdcc29ea0368d78981611ef8f25672c23988
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-01-27 10:13:21 +0500

    .

--- loyiha/admin.py
+++ loyiha/admin.py
@@ -10,19 +10,16 @@ class TexnologiyalarAdmin(admin.ModelAdmin):
 
 
 @admin.register(Loyiha)
-class LoyihaAdmin(admin.ModelAdmin):
+class LoyihaAdmin(TranslationAdmin):
     list_display = ['name', 'created_at']
-# @admin.register(Tajriba)
-# class TajribaAdmin(TranslationAdmin):
-#     list_display = ['name', 'started_at', 'ended_at', 'center']
-#
-#     class Media:
-#         js = (
-#             'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-#             'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-#             'modeltranslation/js/tabbed_translation_fields.js',
-#         )
-#         css = {
-#             'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-#         }
+
+    class Media:
+        js = (
+            'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
 
