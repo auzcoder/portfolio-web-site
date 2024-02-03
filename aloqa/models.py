ed329a9852c588e2d2bbe2b996a21f7055891b0a
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-02-03 17:29:46 +0500

    .

--- aloqa/models.py
+++ aloqa/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    firstname = models.CharField(max_length=200, null=False, blank=False)
+    lastname = models.CharField(max_length=200, null=False, blank=False)
+    email = models.EmailField(max_length=200, null=False, blank=False)
+    message = models.TextField(max_length=1500, null=False, blank=False)
