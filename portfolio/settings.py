ccdb37c2cd244136422ca39145db93c31ea59d8d
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-08-08 10:24:24 +0500

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -172,7 +172,7 @@ MODELTRANSLATION_LANGUAGES = ('uz', 'en')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
